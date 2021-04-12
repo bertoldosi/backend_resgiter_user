f0006d5ba0949e01cda325e48f88dcc5081bf678
bertoldosi <matheus.bertoldosi@gmail.com>
2021-04-11 22:19:13 -0500

    :rocket: Deploy
    Deploy na vercel

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/noderest", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+
+mongoose.connect(
+  "mongodb+srv://systemRegisterUser:admin@cluster0.kjgeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 module.exports = mongoose;
